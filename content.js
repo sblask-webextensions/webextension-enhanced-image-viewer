17dc2b8f444c4b9b7e5b5dbd859cc8d7741990f1
Sebastian Blask <blask@gmx.de>
2017-06-11 00:51:00 +0200

    Add more sizes to switch between when left-clicking

--- content.js
+++ content.js
@@ -1,18 +1,30 @@
-const CSS = `
-    body {
-        background: #000000 !important;
-    }
-    img {
-        cursor: default !important;
-    }
-`;
-
 const STYLE = makeStyle();
+const SIZES = {
+    fit:                      "max-width:  100% !important; max-height: 100% !important;",
+
+    fitToWidthUnlessSmaller:  "max-width:  100% !important; width: auto !important; height: auto !important;",
+    fitToHeightUnlessSmaller: "max-height: 100% !important; width: auto !important; height: auto !important;",
+
+    fitToWidth:               "width: 100% !important; height: auto !important;",
+    fitToHeight:              "width: auto !important; height: 100% !important;",
+
+    noFit:                    "width: auto !important; height: auto !important;",
+};
+
+let sizeStates = Object.keys(SIZES);
+let currentSizeState = sizeStates[0];
 
 function handleClick(event) {
+    if (event.buttons !== 0) {
+        return;
+    }
+
     event.stopImmediatePropagation();
     event.stopPropagation();
     event.preventDefault();
+
+    currentSizeState = sizeStates[(sizeStates.indexOf(currentSizeState) + 1) % sizeStates.length];
+    updateStyle(makeCSS());
 }
 
 function makeStyle() {
@@ -30,6 +42,18 @@ function updateStyle(css) {
     STYLE.appendChild(document.createTextNode(css));
 }
 
-updateStyle(CSS);
+function makeCSS() {
+    return `
+        body {
+            background: #000000 !important;
+        }
+        img {
+            cursor: default !important;
+            ${SIZES[currentSizeState]}
+        }
+    `;
+}
+
+updateStyle(makeCSS());
 
 document.addEventListener("click", handleClick, true);
